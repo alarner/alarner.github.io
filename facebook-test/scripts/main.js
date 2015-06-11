bd866aead8f4819cdad06ac9ce3bfaf3a7b5227d
Aaron Larner <anlarner@gmail.com>
2015-06-11 17:46:04 -0500

    Update facebook

--- facebook-test/scripts/main.js
+++ facebook-test/scripts/main.js
@@ -6,8 +6,19 @@ $(document).ready(function() {
 			version: 'v2.3' // or v2.0, v2.1, v2.0
 		});     
 		// $('#loginbutton,#feedbutton').removeAttr('disabled');
-		FB.getLoginStatus(function(status) {
-			console.log(status);
+		FB.getLoginStatus(function(response) {
+			if(response.status === 'not_authorized') {
+				FB.login(function(response) {
+					 if (response.authResponse) {
+						 console.log('Welcome!  Fetching your information.... ');
+						 FB.api('/me', function(response) {
+							 console.log('Good to see you, ' + response.name + '.');
+						 });
+					 } else {
+						 console.log('User cancelled login or did not fully authorize.');
+					 }
+				 });
+			}
 		});
 	});
 });
