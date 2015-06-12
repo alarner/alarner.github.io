139a0e6bd2f0a4c5d37326d59e33072fe0fb1b84
Aaron Larner <anlarner@gmail.com>
2015-06-12 11:12:32 -0500

    Testing out facebook

--- facebook-test/scripts/main.js
+++ facebook-test/scripts/main.js
@@ -4,27 +4,28 @@ $(document).ready(function() {
 		FB.init({
 			appId: '895741410491942',
 			version: 'v2.3' // or v2.0, v2.1, v2.0
-		});     
-		// $('#loginbutton,#feedbutton').removeAttr('disabled');
-		FB.getLoginStatus(function(response) {
-			if(response.status === 'not_authorized') {
-				FB.login(function(response) {
-					 if (response.authResponse) {
-						 console.log('Welcome!  Fetching your information.... ');
-						 FB.api('/me', function(response) {
-							 console.log('Good to see you, ' + response.name + '.');
-						 });
-					 } else {
-						 console.log('User cancelled login or did not fully authorize.');
-					 }
-				 });
-			}
-			else {
-				FB.api('/me', function(response) {
-					console.log('Good to see you, ' + response.name + '.');
-					console.log(response);
-				});
-			}
 		});
+		FB.getLoginStatus(function(response) {
+			console.log(response);
+		});
+	// 		if(response.status === 'not_authorized') {
+	// 			FB.login(function(response) {
+	// 				 if (response.authResponse) {
+	// 					 console.log('Welcome!  Fetching your information.... ');
+	// 					 FB.api('/me', function(response) {
+	// 						 console.log('Good to see you, ' + response.name + '.');
+	// 					 });
+	// 				 } else {
+	// 					 console.log('User cancelled login or did not fully authorize.');
+	// 				 }
+	// 			 });
+	// 		}
+	// 		else {
+	// 			FB.api('/me', function(response) {
+	// 				console.log('Good to see you, ' + response.name + '.');
+	// 				console.log(response);
+	// 			});
+	// 		}
+	// 	});
 	});
 });
