e01e2a174ff43d63a3c564aeae1832a05a8fd9ae
Aaron Larner <anlarner@gmail.com>
2016-03-09 07:30:34 -0600

    Update dash.js

--- scripts/dash.js
+++ scripts/dash.js
@@ -1,1 +1,9 @@
-console.log(window.location);
+if(window.location.hostname !== 'online.theironyard.com') {
+	alert('This bookmarklet must be run from TIYO');
+}
+else if(!window.location.pathname.match(/\/admin\/paths\/([0-9]+)/)) {
+	alert('Please go to the path that you\'d like to summarize.');
+}
+else {
+	console.log('You\'re on the correct page');
+}
