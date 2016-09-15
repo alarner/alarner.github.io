d3b4facfef7a997b5a2902905b78c1526f4a427e
Aaron Larner <alarner@trello.com>
2016-09-15 14:30:18 -0400

    Update canned responses

--- canned-responses/scripts/main.js
+++ canned-responses/scripts/main.js
@@ -1,3 +1,42 @@
+var parkMap = {
+  acad: 'Acadia National Park',
+  arch: 'Arches National Park',
+  badl: 'Badlands National Park',
+  brca: 'Bryce Canyon National Park',
+  crla: 'Crater Lake National Park',
+  dena: 'Denali National Park',
+  glac: 'Glacier National Park',
+  grca: 'Grand Canyon National Park',
+  grte: 'Grand Teton National Park',
+  olym: 'Olympic National Park',
+  yell: 'Yellowstone National Park',
+  yose: 'Yosemite National Park',
+  zion: 'Zion National Park'
+};
+
+var cardButtonCallback = function(t){
+  var items = Object.keys(parkMap).map(function(parkCode){
+    var urlForCode = 'http://www.nps.gov/' + parkCode + '/';
+    return {
+      text: parkMap[parkCode],
+      url: urlForCode,
+      callback: function(t){
+        return t.closePopup();
+      }
+    };
+  });
+
+  return t.popup({
+    title: 'Popup Search Example',
+    items: items,
+    search: {
+      count: 5,
+      placeholder: 'Search National Parks',
+      empty: 'No parks found'
+    }
+  });
+};
+
 TrelloPowerUp.initialize({
   'card-buttons': function(t, options) {
     return [{
