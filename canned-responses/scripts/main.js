53dda8439844220200e221f7e124152da80706cb
Aaron Larner <alarner@trello.com>
2016-09-15 14:23:44 -0400

    Update canned responses

--- canned-responses/scripts/main.js
+++ canned-responses/scripts/main.js
@@ -1,15 +1,15 @@
 TrelloPowerUp.initialize({
-  'card-buttons': function(t, options){
-    return [
-      {
-        text: 'Canned Responses',
-        callback: function(t) {
-          return t.overlay({
-            url: './templates.html',
-            args: { rand: (Math.random() * 100).toFixed(0) }
-          });
-        }
-      }
-    ];
-  }
+  // 'card-buttons': function(t, options){
+  //   return [
+  //     {
+  //       text: 'Canned Responses',
+  //       callback: function(t) {
+  //         return t.overlay({
+  //           url: './templates.html',
+  //           args: { rand: (Math.random() * 100).toFixed(0) }
+  //         });
+  //       }
+  //     }
+  //   ];
+  // }
 });
